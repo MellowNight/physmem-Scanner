7494e99a2202c66b5b0c7ea698ddfa56f5adb419
sp000fer <66788741+sp000fer@users.noreply.github.com>
2020-09-08 15:04:17 -0400

    some change

--- spooferTool/smbios.h
+++ spooferTool/smbios.h
@@ -9,5 +9,12 @@
 
 void	spoofSMBIOS()
 {
+	INPUT_STRUCT	input;
 
+	DWORD			bytesReturn;
+
+	BOOL	status = DeviceIoControl(Globals::driverHandle, SPOOF_SMBIOS, &input,
+		sizeof(INPUT_STRUCT), 0, 0, &bytesReturn, 0);
+
+	return;
 }
